40ed1e68abde17ffcfc27eb9a0d8a72ceafe3364
Anthony Ruffin <aruffincs@gmail.com>
2018-03-29 16:21:27 -0400

    commented out code to close modal

---
+++ public/index.js
@@ -0,0 +1,15 @@
+// // Function to close modal if open
+// const closeModal = () => {
+//     // Get modal ID
+//     const hideModal = document.getElementById('#modal');
+//     const isModalVisible = document.getElementById('#modal').style.display = 'block';
+
+//     hideModal.style.display = 'none';
+// };
+
+// //Use escape key to exit modal
+// document.addEventListener('keypress', (e) => {
+//     if(e.keycode == 27) {
+//         closeModal();
+//     }
+// })
